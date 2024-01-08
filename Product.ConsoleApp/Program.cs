5184ebc4f28d91662c2455ccbcab2a5cd522c3a5
Onur <onurda@code.edu.az>
2024-01-09 01:03:53 +0400

    created console menu , directory and file

--- Product.ConsoleApp/Program.cs
+++ Product.ConsoleApp/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string appStart = "Application started...";
+string Welcome = "Welcome!";
+Console.SetCursorPosition((Console.WindowWidth - appStart.Length) / 2, Console.CursorTop);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(appStart);
+Console.SetCursorPosition((Console.WindowWidth - Welcome.Length) / 2, Console.CursorTop);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(Welcome);
+Console.ResetColor();
+
+Directory.CreateDirectory(@".\ProductDB");
+if (!File.Exists(@".\ProductDB\product.txt"))
+{
+    File.Create(@".\ProductDB\product.txt");
+}
+bool runApp = true;
+while (runApp)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("1 == Create product\n"+
+                      "2 == Get list products\n"+
+                      "0 == Close app\n"+
+                      " ");
+    Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write("Chosse the option >> ");
+    Console.ResetColor();
+    Console.ReadLine();
+}
